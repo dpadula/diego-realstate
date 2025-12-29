bbbf842579695e475c128ea39a0655944df1910d
Diego <dpadula@epe.santafe.gov.ar>
2025-12-29 15:09:42 -0300

    tipado para TS de los tipos de archivo de imagenes

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -10,7 +10,10 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text className='text-xl font-bold my-10'>Welcome to the ReState</Text>
+      <Text className='text-3xl font-bold my-10 font-rubik'>
+        Welcome to the ReState
+      </Text>
+      <Text className='font-rubik'>Go to SignIn</Text>
       <Link href='/signin'>Go to SignIn</Link>
       <Link href='/explore'>Go to Explore</Link>
       <Link href='/profile'>Go to Profile</Link>
