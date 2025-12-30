581a23d6baf905d59c5521780b549b7060f2632c
Diego <dpadula@epe.santafe.gov.ar>
2025-12-30 09:49:11 -0300

    esto hace scroll en el  scrollview, cuando las alturas de las imagenes son fijas (si se pone algun ratio como h-4/6 o cualquier otra relativa no anda)

--- src/app/signin.tsx
+++ src/app/signin.tsx
@@ -1,17 +1,98 @@
 import images from '@constants/images';
 import React from 'react';
-import { Image, ScrollView, StyleSheet } from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import icons from '../../constants/icons';
 
 const SignIn = () => {
+  const handleLogin = async () => {
+    // const result = await login();
+    // if (result) {
+    //   refetch();
+    // } else {
+    //   Alert.alert('Error', 'Failed to login');
+    // }
+    console.log('Login button pressed');
+  };
   return (
-    <SafeAreaView className='bg-white h-full'>
-      <ScrollView contentContainerClassName='h-full'>
+    <SafeAreaView className='bg-white flex-1'>
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        contentContainerClassName='px-10 pb-8'
+        showsVerticalScrollIndicator={false}
+      >
         <Image
           source={images.onboarding}
-          className='w-full h-4/6'
+          className='w-full h-96'
           resizeMode='contain'
         />
+        <Image
+          source={images.onboarding}
+          className='w-full h-96'
+          resizeMode='contain'
+        />
+        <Text className='text-base text-center uppercase font-rubik text-black-200'>
+          Welcome To Real Scout
+        </Text>
+        <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+          Let´s Get You Closer To {'\n'}
+          <Text className='text-primary-300'>Your Ideal Home</Text>
+        </Text>
+        <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
+          Login to Real Scout with Google
+        </Text>
+        <TouchableOpacity
+          onPress={handleLogin}
+          className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'
+        >
+          <View className='flex flex-row items-center justify-center'>
+            <Image
+              source={icons.google}
+              className='w-5 h-5'
+              resizeMode='contain'
+            />
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>
+              Continue with Google
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleLogin}
+          className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'
+        >
+          <View className='flex flex-row items-center justify-center'>
+            <Image
+              source={icons.google}
+              className='w-5 h-5'
+              resizeMode='contain'
+            />
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>
+              Continue with Google
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleLogin}
+          className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'
+        >
+          <View className='flex flex-row items-center justify-center'>
+            <Image
+              source={icons.google}
+              className='w-5 h-5'
+              resizeMode='contain'
+            />
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>
+              Continue with Google
+            </Text>
+          </View>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
